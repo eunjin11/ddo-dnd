53db877da4651f8193d39f4928aad41f804a7c47
eunjin11 <whdmswls9207@gmail.com>
2025-11-19 15:57:37 +0900

    fix/#6: parserOptions 위치 수정

--- apps/playground/eslint.config.js
+++ apps/playground/eslint.config.js
@@ -4,6 +4,7 @@ import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import tseslint from 'typescript-eslint';
 import { defineConfig, globalIgnores } from 'eslint/config';
+import { fileURLToPath } from 'node:url';
 
 export default defineConfig([
   globalIgnores(['dist']),
@@ -18,9 +19,9 @@ export default defineConfig([
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
-    },
-    parserOptions: {
-      tsconfigRootDir: new URL('.', import.meta.url),
+      parserOptions: {
+        tsconfigRootDir: fileURLToPath(new URL('.', import.meta.url)),
+      },
     },
   },
 ]);
